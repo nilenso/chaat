cbfea0697328659d688829c563e3157e512dcfc1
shahn95 <shahn@nilenso.com>
2023-06-19 15:10:45 +0530

    Update handler.clj with handlers for signup and delete-account

--- src/chaat/handler.clj
+++ src/chaat/handler.clj
@@ -1,6 +1,11 @@
 (ns chaat.handler
   (:require [ring.util.response :as res]
-            [clj-time.local :as l]))
+            [clj-time.local :as l]
+            [chaat.model.user :as user]))
+
+;; validation for not null can be done here
+;; Check for get/post/put
+;; controller layer -> handler
 
 (defn home
   [request]
@@ -11,6 +16,25 @@
   (->> (str "Service is running: " (l/local-now))
        res/response))
 
+(defn signup
+  [request]
+  (let [request-method (:request-method request)
+        params (:params request)
+        username (:user params)
+        password (:password params)]
+    (if (and username password (user/create-user username password))
+      (res/response (str "Successfully created " username " account."))
+      (res/bad-request (str "Error creating " username " account.")))))
+
+(defn delete-account
+  [request]
+  (let [request-method (:request-method request)
+        params (:params request)
+        username (:user params)]
+    (if (and username (user/delete-user username))
+      (res/response (str "Account deleted"))
+      (res/bad-request (str "Account does not exist")))))
+
 (defn test-page
   [request]
   (res/response (str request)))
@@ -18,4 +42,3 @@
 (defn not-found
   [request]
   (res/not-found "Resource does not exist"))
-
