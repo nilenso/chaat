e80122f2cf4402c1f9e593d3ee19a96ece89c0c1
shahn95 <shahn@nilenso.com>
2023-06-29 17:08:41 +0530

    Update dependencies

--- project.clj
+++ project.clj
@@ -7,15 +7,14 @@
                  [ring/ring-core "1.10.0"]
                  [ring/ring-jetty-adapter "1.10.0"]
                  [ring/ring-devel "1.10.0"]
+                 [ring/ring-json "0.5.1"]
                  [bidi "2.1.6"]
-                 [clj-time "0.15.2"]
                  [ragtime "0.8.0"]
-                 [org.clojure/java.jdbc "0.7.12"]
                  [org.postgresql/postgresql "42.6.0"]
                  [crypto-password "0.3.0"]
-                 [seancorfield/next.jdbc "1.2.659"]
+                 [crypto-random "1.2.1"]
                  [com.github.seancorfield/next.jdbc "1.3.874"]
-                 [com.github.seancorfield/honeysql "2.4.1033"]]
+                 [clojure.java-time "1.2.0"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler chaat.handler/handler}
   :main ^:skip-aot chaat.app
