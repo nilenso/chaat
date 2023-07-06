4dd282607b280bfbdb5e33bc22e72550216565c6
shahn95 <shahn@nilenso.com>
2023-07-06 13:30:03 +0530

    Update to use callable functions: run-migrations and rollback

--- src/chaat/migrations.clj
+++ src/chaat/migrations.clj
@@ -3,13 +3,19 @@
             [ragtime.repl :as repl]
             [chaat.config :as config]))
 
-;; changed to def from defn
-(def config {:datastore (ragtime.jdbc/sql-database (:pg-db config/app-config))
-             :migrations (ragtime.jdbc/load-resources "migrations")})
+(defn gen-config-map
+  [dbspec]
+  {:datastore (ragtime.jdbc/sql-database dbspec)
+   :migrations (ragtime.jdbc/load-resources "migrations")})
 
-(repl/rollback config)
-(repl/migrate config)
+(defn rollback []
+  (repl/rollback (gen-config-map (config/get-pg-dbspec))))
+
+(defn run-migrations []
+  (repl/migrate (gen-config-map (config/get-pg-dbspec))))
 
 (comment
   {:connection-uri
    "jdbc:postgresql://127.0.0.1:8001/chaat-db?user=chaat-dev&password=password"})
+
+
