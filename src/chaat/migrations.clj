1b9e434f8930940d62155c8c16b69feaff262a7c
shahn95 <shahn@nilenso.com>
2023-07-14 21:57:02 +0530

    Change run-migrations and rollback fns to take dbspec as an argument
    Will improve these further in a future commit.

--- src/chaat/migrations.clj
+++ src/chaat/migrations.clj
@@ -3,19 +3,19 @@
             [ragtime.repl :as repl]
             [chaat.config :as config]))
 
+;; using the migration functions in the ragtime.repl namespace for now
+
 (defn gen-config-map
   [dbspec]
   {:datastore (ragtime.jdbc/sql-database dbspec)
    :migrations (ragtime.jdbc/load-resources "migrations")})
 
-(defn rollback []
-  (repl/rollback (gen-config-map (config/get-pg-dbspec))))
+(defn rollback [dbspec]
+  (repl/rollback (gen-config-map dbspec)))
 
-(defn run-migrations []
-  (repl/migrate (gen-config-map (config/get-pg-dbspec))))
+(defn run-migrations [dbspec]
+  (repl/migrate (gen-config-map dbspec)))
 
 (comment
   {:connection-uri
-   "jdbc:postgresql://127.0.0.1:8001/chaat-db?user=chaat-dev&password=password"})
-
-
+   "jdbc:postgresql://127.0.0.1:8001/chaat_db?user=chaat_dev&password=password"})
