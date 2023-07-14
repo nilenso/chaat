5b6cdab6ae0222153ab0f070345cb106f7fa6e51
shahn95 <shahn@nilenso.com>
2023-07-14 22:16:33 +0530

    Initialize fixture namespace with test-system and test-fixture for running tests

---
+++ test/chaat/fixture.clj
@@ -0,0 +1,36 @@
+(ns chaat.fixture
+  (:require [chaat.config :as config]
+            [com.stuartsierra.component :as component]
+            [next.jdbc.connection :as conn]
+            [chaat.app :as app]
+            [ragtime.jdbc]
+            [ragtime.repl]
+            [chaat.migrations :as migrations])
+  (:import (com.zaxxer.hikari HikariDataSource)))
+
+(defn new-test-system [config]
+  (component/system-map
+   :server (component/using (app/new-http-server config) {:db :db})
+   :db (conn/component HikariDataSource (:pg-test-dbspec config))))
+
+(def test-system-config {:options {:port (config/get-local-test-port)
+                                   :join? false}
+                         :pg-test-dbspec (config/get-pg-test-dbspec)})
+
+(def test-system (new-test-system test-system-config))
+
+(defn start-test-system []
+  (alter-var-root #'test-system component/start))
+
+(defn stop-test-system []
+  (alter-var-root #'test-system component/stop))
+
+;; fixture with test-system (server + db) and migrations run/rollback
+(defn test-fixture [tests]
+  (stop-test-system)
+  (start-test-system)
+  (migrations/rollback (config/get-pg-test-dbspec))
+  (migrations/run-migrations (config/get-pg-test-dbspec))
+  (tests)
+  (migrations/rollback (config/get-pg-test-dbspec))
+  (stop-test-system))
