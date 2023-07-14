956433b521182565e24a3ee95cb06d4d6e6bf2d9
shahn95 <shahn@nilenso.com>
2023-07-14 21:59:43 +0530

    Add a user-exists? fn

--- src/chaat/db/user.clj
+++ src/chaat/db/user.clj
@@ -3,10 +3,17 @@
    [next.jdbc :as jdbc]
    [next.jdbc.sql :as sql]))
 
+;; these 2 functions do the same thing, will remove one of them
 (defn new-user?
   "Check if username exists in username column of user table."
   [datasource username]
   (empty? (sql/find-by-keys datasource :users {:username username})))
+
+(defn user-exists?
+  [datasource username]
+  (let [result (sql/find-by-keys datasource :users {:username username})]
+    (if (seq result)
+      true false)))
 
 (defn insert
   "Insert new user into user table"
@@ -20,7 +27,8 @@
     (catch Exception e
       {:result nil :error (str "Postgres exception: " e)})))
 
-;; future: authenticate users as themselves before deleting
+;; future: add authentication/authorization for deleting
+;; users need to be authenticated as username to gain authorization to delete username
 (defn delete
   "Remove user from user table"
   [db username]
@@ -29,8 +37,3 @@
     (if (= 1 update-count)
       {:result username :error nil}
       {:result nil :error "Error deleting user"})))
-
-;; Repl commands
-;; (sql/delete! (pg-ds) :users {:username "udit"})
-;; (sql/insert! (pg-ds) :users {:username "udit"
-;;                            :password_hash "123"})
