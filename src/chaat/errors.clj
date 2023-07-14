23f13c78b516564999fba5066e79fa6f47aed803
shahn95 <shahn@nilenso.com>
2023-07-14 21:56:14 +0530

    Improve docstring for fn do-or-error and add comments

--- src/chaat/errors.clj
+++ src/chaat/errors.clj
@@ -6,11 +6,15 @@
    cascade down the remaining expressions in the let block without performing 
    the remaining operations.
 
-   In situations where f does not return a result map, it is usually when the 
-   operation is not a validation or db query. Hence, one may not be concerned 
-   about checking that particular output of f before continuing. In this case, 
-   use the result map of the previous validation/query to continue the chain."
+   In situations where function f does not return a result map, it is usually 
+   when the operation is not a validation or db query. Here, one may not be 
+   concerned about checking that particular output of f before continuing, and 
+   we use the result map of the previous validation/query to continue the chain."
   [{:keys [result error]} f & args]
   (if (nil? error)
     (apply f args)
     {:result result :error error}))
+
+;; consider creating standard error messages. maybe in a map structure.
+;; (def invalid-username "Wrong username format")
+;; (def invalid-password "Wrong password format")
