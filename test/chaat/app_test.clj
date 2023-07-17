989daf188b58710df338611e7e4196c717939cdc
shahn95 <shahn@nilenso.com>
2023-07-17 17:14:53 +0530

    Initialize app_test.clj with end-end health check test

---
+++ test/chaat/app_test.clj
@@ -0,0 +1,12 @@
+(ns chaat.app-test
+  (:require [chaat.fixture :as fixture]
+            [clojure.test :refer :all]
+            [clj-http.client :as client]))
+
+(use-fixtures :each fixture/test-fixture)
+
+(deftest end-end-test
+  (testing "get request to health check returns status 200"
+    (let [address "http://0.0.0.0:3010/health-check"
+          response (client/get address)]
+      (is (= 200 (:status response))))))
