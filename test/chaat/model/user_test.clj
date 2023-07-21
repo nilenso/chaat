6a04f6baba1d82ec80f0983de139680898ee98ff
shahn95 <shahn@nilenso.com>
2023-07-21 20:29:00 +0530

    Add tests for authenticate and login fns in model layer

--- test/chaat/model/user_test.clj
+++ test/chaat/model/user_test.clj
@@ -5,7 +5,9 @@
             [chaat.fixture :as fixture]
             [java-time.api :as jt]
             [crypto.password.bcrypt :as bcrypt]
-            [chaat.handler.errors :refer [error-table]]))
+            [chaat.handler.errors :refer [error-table]]
+            [buddy.sign.jwt :as jwt]
+            [chaat.config :as config]))
 
 (use-fixtures :each fixture/test-fixture)
 
@@ -56,6 +58,62 @@
               password "12345678"
               actual-result (model.user/create datasource username password)
               expected-result {:result nil :error (:username-exists error-table)}]
+          (is (= expected-result actual-result)))))))
+
+(deftest authenticate-test
+  (let [username "john"
+        password "12345678"
+        work-factor 11
+        retrieved-password-hash (bcrypt/encrypt password work-factor)
+        payload {:sub nil
+                 :username username
+                 :iat nil
+                 :eat nil}
+        token (jwt/sign payload (config/get-secret))]
+
+    (testing "Return map with JWT when password hashes match"
+      (let [expected-result {:result {:jwt token} :error nil}
+            actual-result (model.user/authenticate retrieved-password-hash username password)]
+        (is (= expected-result actual-result))))
+
+    (testing "Return map with error when password hashed do not match"
+      (let [password "12312312"
+            actual-result (model.user/authenticate retrieved-password-hash username password)
+            expected-result {:result nil :error "Username or password is incorrect"}]
+        (is (= expected-result actual-result))))))
+
+(deftest login-test
+  (let [datasource (:db fixture/test-system)
+        username "john"
+        password "12345678"
+        payload {:sub nil
+                 :username username
+                 :iat nil
+                 :eat nil}
+        token (jwt/sign payload (config/get-secret))]
+
+    (testing "JWT is not generated for invalid credentials"
+      (let [username "j"
+            actual-result (model.user/login datasource username password)
+            expected-result {:result nil :error "Wrong username format"}]
+        (is (= expected-result actual-result))))
+
+    (testing "JWT is not generated for user that does not exist"
+      (let [actual-result (model.user/login datasource username password)
+            expected-result {:result nil :error "Username or password is incorrect"}]
+        (is (= expected-result actual-result))))
+
+    ;; create user john
+    (let [_ (model.user/create datasource username password)]
+      (testing "JWT is not generated for existing user with wrong password"
+        (let [password "12312312"
+              actual-result (model.user/login datasource username password)
+              expected-result {:result nil :error "Username or password is incorrect"}]
+          (is (= expected-result actual-result))))
+
+      (testing "JWT is generated for existing user with correct password"
+        (let [actual-result (model.user/login datasource username password)
+              expected-result {:result {:jwt token} :error nil}]
           (is (= expected-result actual-result)))))))
 
 (deftest delete-test
