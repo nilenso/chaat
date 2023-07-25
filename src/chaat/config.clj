1f09b90493cd01a204d1f85c89c74d68f5211798
shahn95 <shahn@nilenso.com>
2023-07-25 18:07:54 +0530

    Improve get config fns

--- src/chaat/config.clj
+++ src/chaat/config.clj
@@ -3,26 +3,32 @@
             [clojure.edn :as edn])
   (:import (java.io PushbackReader)))
 
-;; change defn's -> def's, not much of an advantage here
+(def config (atom nil))
 
-(defn get-app-config []
+(defn reload-config []
   (with-open [f (io/reader "./resources/config.edn")]
     (->> f
          (PushbackReader.)
-         (edn/read))))
+         (edn/read)
+         (reset! config))))
+
+(defn get-config []
+  (if @config
+    @config
+    (reload-config)))
 
 (defn get-pg-dbspec []
-  (:pg-dbspec (get-app-config)))
+  (:pg-dbspec (get-config)))
 
 (defn get-pg-test-dbspec []
-  (:pg-test-dbspec (get-app-config)))
+  (:pg-test-dbspec (get-config)))
 
 (defn get-local-port []
-  (:local-port (:app (get-app-config))))
+  (:local-port (:app (get-config))))
 
 (defn get-local-test-port []
-  (:local-test-port (:app (get-app-config))))
+  (:local-test-port (:app (get-config))))
 
 (comment
   (edn/read (PushbackReader. (io/reader "./resources/config.edn")))
-  (new PushbackReader (io/reader "./resources/config.edn")))
+  (edn/read (new PushbackReader (io/reader "./resources/config.edn"))))
