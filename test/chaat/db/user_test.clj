7a9465f94473b619d459c03812de0132e22e4d70
shahn95 <shahn@nilenso.com>
2023-07-27 15:03:56 +0530

    Remove get-password-hash-test and add get-user-details test

--- test/chaat/db/user_test.clj
+++ test/chaat/db/user_test.clj
@@ -35,7 +35,7 @@
           (is (= true (db.user/user-exists? (datasource) "john")))
           (is (= expected-result actual-result-without-id))))
 
-      ;; depends on prior state. put into different block?
+      ;; depends on prior state. will put these in different deftest blocks in the
       (testing "Insert existing user into user table fails"
         (let [user-info (model.user/gen-new-user username password)
               actual-result (db.user/insert datasource user-info)
@@ -60,21 +60,28 @@
         (is (= expected-result actual-result))
         (is (= false (db.user/user-exists? (datasource) username)))))))
 
-(deftest get-password-hash-test
-  (with-redefs [bcrypt/encrypt encrypt-stub]
+(deftest get-user-details-test
+  (with-redefs [bcrypt/encrypt encrypt-stub
+                model.user/get-time-instant time-stub]
     (let [datasource (:db fixture/test-system)
           username "john"
           password "12345678"
-          user-info (model.user/gen-new-user-map username password)]
+          user-info (model.user/gen-new-user username password)]
 
       (testing "Return map containing error info when user does not exist"
-        (let [actual-result (db.user/get-password-hash datasource username)
-              expected-result {:result nil :error "Username or password is incorrect"}]
+        (let [actual-result (db.user/get-user-details datasource username)
+              expected-result {:result nil :error (:username-not-exists error-table)}]
           (is (= expected-result actual-result))))
 
-      (testing "Return map containing password hash when user exists"
+      (testing "Return map containing user details when user exists"
         (let [_ (db.user/insert datasource user-info)
-              actual-result (db.user/get-password-hash datasource username)
-              expected-pwd-hash "$2a$11$DoWjFwnL5glpyGqBRgdA3uqoy1glTFVoXP.wesem27g2SL3XFXOHW"
-              expected-result {:result expected-pwd-hash :error nil}]
-          (is (= expected-result actual-result)))))))
+              actual-result (db.user/get-user-details datasource username)
+              actual-result-without-id (update-in actual-result [:result] dissoc :users/id)
+              expected-result {:result
+                               #:users{:username "john",
+                                       :password_hash
+                                       "$2a$11$DoWjFwnL5glpyGqBRgdA3uqoy1glTFVoXP.wesem27g2SL3XFXOHW",
+                                       :creation_timestamp
+                                       #inst "2023-06-13T10:07:03.172000000-00:00"},
+                               :error nil}]
+          (is (= expected-result actual-result-without-id)))))))
